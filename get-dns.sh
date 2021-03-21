3c436fa60f389fc88e6c512df4ce4c346f6d2fe2
Nathan Rzepecki <nathan@lionslair.net.au>
2021-03-21 20:51:05 +0800

    Update the script so it gets the maximum number of records. was limited to 20 by defualt now gets 200

--- get-dns.sh
+++ get-dns.sh
@@ -7,6 +7,7 @@ response=$(curl \
   -X GET \
   -H "Content-Type: application/json" \
   -H "Authorization: Bearer $ACCESS_TOKEN" \
-  "https://api.digitalocean.com/v2/domains/$DOMAIN/records")
+  "https://api.digitalocean.com/v2/domains/$DOMAIN/records?per_page=200")
 
 echo $response | grep -Eo '"id":\d*|"type":"\w*"|"data":".*?"'
+# echo $response
